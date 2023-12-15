1a9fe5024ad56930421db3a47ba3bc2754cf11d1
DHRMKJ <dhrmk@DHARMIK>
2023-12-15 16:36:21 +0530

    format

--- src/stories.h
+++ src/stories.h
@@ -14,34 +14,34 @@ const char *stories[] = {
 	"Pablo, a budding artist, found a magical paintbrush that brought his drawings to life. From friendly doodles to whimsical characters, Pablo and his colorful creations embarked on adventures in the painted world. Each stroke of the paintbrush opened new portals to imaginative realms, and soon, Pablo's Paintbrush Pals became the talk of the town. Join Pablo as he explores the boundaries of creativity, where every canvas is a gateway to endless possibilities."
 	,
 
-	 "Tommy stumbled upon a mysterious island made entirely of ice cream. Vanilla sands stretched along the shore, while chocolate mountains loomed in the distance. Each flavor had a unique power – bubblegum bounce, strawberry swing, and even chocolate teleportation! Tommy decided to embark on sweet adventures, discovering the secrets of each flavor and sharing the magic with newfound ice cream friends."
+	"Tommy stumbled upon a mysterious island made entirely of ice cream. Vanilla sands stretched along the shore, while chocolate mountains loomed in the distance. Each flavor had a unique power – bubblegum bounce, strawberry swing, and even chocolate teleportation! Tommy decided to embark on sweet adventures, discovering the secrets of each flavor and sharing the magic with newfound ice cream friends."
     	,
 
-    "Bella, a creative bee in the garden, had a dream – a bumblebee ballet! She organized auditions, and soon the garden buzzed with excitement. Bees practiced their twirls, pirouettes, and synchronized flights, creating a buzzworthy dance that left flowers applauding. The Bumblebee Ballet became a seasonal spectacle, with bees and flowers alike eagerly anticipating the garden's annual dance extravaganza."
+   	"Bella, a creative bee in the garden, had a dream – a bumblebee ballet! She organized auditions, and soon the garden buzzed with excitement. Bees practiced their twirls, pirouettes, and synchronized flights, creating a buzzworthy dance that left flowers applauding. The Bumblebee Ballet became a seasonal spectacle, with bees and flowers alike eagerly anticipating the garden's annual dance extravaganza."
     	,
 
-    "Charlie stumbled upon a magic cactus in the desert, turning the sandy landscape into a vibrant carnival at night. Neon lights adorned the cacti, and carnival rides popped up amidst the dunes. The cacti danced, scorpions sang, and rollercoaster tumbleweeds rolled through the sandy carnival grounds. The Cactus Carnival became a yearly celebration, attracting visitors from far and wide to experience the enchanting desert festivities."
+    	"Charlie stumbled upon a magic cactus in the desert, turning the sandy landscape into a vibrant carnival at night. Neon lights adorned the cacti, and carnival rides popped up amidst the dunes. The cacti danced, scorpions sang, and rollercoaster tumbleweeds rolled through the sandy carnival grounds. The Cactus Carnival became a yearly celebration, attracting visitors from far and wide to experience the enchanting desert festivities."
     	,
 
-    "Jenny, a young inventor, created jetpacks fueled by jellybeans. Kids in the neighborhood strapped on these sweet-powered devices and zoomed through the sky. Loop-de-loops, jellybean trails, and laughter filled the air as they soared above the clouds. The Jellybean Jetpacks became a sensation, turning ordinary afternoons into sugary adventures that left everyone with smiles and pockets full of jellybeans."
+    	"Jenny, a young inventor, created jetpacks fueled by jellybeans. Kids in the neighborhood strapped on these sweet-powered devices and zoomed through the sky. Loop-de-loops, jellybean trails, and laughter filled the air as they soared above the clouds. The Jellybean Jetpacks became a sensation, turning ordinary afternoons into sugary adventures that left everyone with smiles and pockets full of jellybeans."
     	,
 
-    "Emily discovered a secret meadow where unicorns roamed freely. One sunny day, she organized a magical picnic with the unicorns. Rainbow cupcakes, cotton candy clouds, and giggles filled the air as they feasted and danced. The Unicorn Picnic became an annual tradition, a day of enchantment where children and unicorns shared laughter under the sparkling sun."
+    	"Emily discovered a secret meadow where unicorns roamed freely. One sunny day, she organized a magical picnic with the unicorns. Rainbow cupcakes, cotton candy clouds, and giggles filled the air as they feasted and danced. The Unicorn Picnic became an annual tradition, a day of enchantment where children and unicorns shared laughter under the sparkling sun."
   	,
 
-  "Tommy stumbled upon a mysterious island made entirely of ice cream. Vanilla sands stretched along the shore, while chocolate mountains loomed in the distance. Each flavor had a unique power – bubblegum bounce, strawberry swing, and even chocolate teleportation! Tommy decided to embark on sweet adventures, discovering the secrets of each flavor and sharing the magic with newfound ice cream friends."
+  	"Tommy stumbled upon a mysterious island made entirely of ice cream. Vanilla sands stretched along the shore, while chocolate mountains loomed in the distance. Each flavor had a unique power – bubblegum bounce, strawberry swing, and even chocolate teleportation! Tommy decided to embark on sweet adventures, discovering the secrets of each flavor and sharing the magic with newfound ice cream friends."
   	,
 
-  "Bella, a creative bee in the garden, had a dream – a bumblebee ballet! She organized auditions, and soon the garden buzzed with excitement. Bees practiced their twirls, pirouettes, and synchronized flights, creating a buzzworthy dance that left flowers applauding. The Bumblebee Ballet became a seasonal spectacle, with bees and flowers alike eagerly anticipating the garden's annual dance extravaganza."
+  	"Bella, a creative bee in the garden, had a dream – a bumblebee ballet! She organized auditions, and soon the garden buzzed with excitement. Bees practiced their twirls, pirouettes, and synchronized flights, creating a buzzworthy dance that left flowers applauding. The Bumblebee Ballet became a seasonal spectacle, with bees and flowers alike eagerly anticipating the garden's annual dance extravaganza."
 	,
 
-  "Charlie stumbled upon a magic cactus in the desert, turning the sandy landscape into a vibrant carnival at night. Neon lights adorned the cacti, and carnival rides popped up amidst the dunes. The cacti danced, scorpions sang, and rollercoaster tumbleweeds rolled through the sandy carnival grounds. The Cactus Carnival became a yearly celebration, attracting visitors from far and wide to experience the enchanting desert festivities."
+  	"Charlie stumbled upon a magic cactus in the desert, turning the sandy landscape into a vibrant carnival at night. Neon lights adorned the cacti, and carnival rides popped up amidst the dunes. The cacti danced, scorpions sang, and rollercoaster tumbleweeds rolled through the sandy carnival grounds. The Cactus Carnival became a yearly celebration, attracting visitors from far and wide to experience the enchanting desert festivities."
   	,
 	
-  "Jenny, a young inventor, created jetpacks fueled by jellybeans. Kids in the neighborhood strapped on these sweet-powered devices and zoomed through the sky. Loop-de-loops, jellybean trails, and laughter filled the air as they soared above the clouds. The Jellybean Jetpacks became a sensation, turning ordinary afternoons into sugary adventures that left everyone with smiles and pockets full of jellybeans."
+  	"Jenny, a young inventor, created jetpacks fueled by jellybeans. Kids in the neighborhood strapped on these sweet-powered devices and zoomed through the sky. Loop-de-loops, jellybean trails, and laughter filled the air as they soared above the clouds. The Jellybean Jetpacks became a sensation, turning ordinary afternoons into sugary adventures that left everyone with smiles and pockets full of jellybeans."
   	,
 
-  "Emily discovered a secret meadow where unicorns roamed freely. One sunny day, she organized a magical picnic with the unicorns. Rainbow cupcakes, cotton candy clouds, and giggles filled the air as they feasted and danced. The Unicorn Picnic became an annual tradition, a day of enchantment where children and unicorns shared laughter under the sparkling sun."
+  	"Emily discovered a secret meadow where unicorns roamed freely. One sunny day, she organized a magical picnic with the unicorns. Rainbow cupcakes, cotton candy clouds, and giggles filled the air as they feasted and danced. The Unicorn Picnic became an annual tradition, a day of enchantment where children and unicorns shared laughter under the sparkling sun."
 };
 
 const size_t stories_count = sizeof(stories) / sizeof(stories[0]);
